cd9f03ac49a5f53a0598e89b4e0757d3b5a692f1
ochaghlil <47222896+ochaghlil@users.noreply.github.com>
2020-11-29 18:23:29 -0500

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,28 @@
+<?php
+	
+session_start();
+header("Location: logIn.php");
+
+	$con = mysqli_connect('localhost', 'root', 'password', 'Dining-Out');
+	
+	
+	$id = mysqli_real_escape_string($con, $_POST['id']);
+	$name = mysqli_real_escape_string($con, $_POST['name']);
+	
+	$query = "SELECT * FROM Employee where Employee_ID = '$id'";
+	
+	$result = mysqli_query($con, $query);
+	
+	$num = mysqli_num_rows($result);
+	
+	if ($num == 1){
+		echo "Username Already Taken";
+	}
+	else {
+		$reg = "INSERT INTO Employee(Employee_ID, Name) values ('$id', '$name')";
+		mysqli_query($con, $reg);
+		echo("Registration Successful");
+	}
+	
+	
+?>
